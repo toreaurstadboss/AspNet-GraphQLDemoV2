7e02c913aff94a165bb06e965f4cf27e05377b67
Tore Aurstad <tore.aurstad@hemit.no>
2022-10-17 00:57:53 +0200

    More adjustments

--- Client/AspNetGraphQLDemoV2.Client/Program.cs
+++ Client/AspNetGraphQLDemoV2.Client/Program.cs
@@ -8,5 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddDemoClient();
+builder.Services.AddGraphQLDemoClient().ConfigureHttpClient(client => client.BaseAddress = new Uri("http:localhost:7074/graphql"));
 await builder.Build().RunAsync();
