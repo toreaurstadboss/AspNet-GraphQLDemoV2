abc2e26de8a284ce4b830a94eee547e9f005005f
Tore Aurstad <tore.aurstad@hemit.no>
2022-10-16 23:43:36 +0200

    Added client blazor

--- Backend/AspNet-GraphQLDemoV2.Server/GraphQL/Types/Queries/MountainQueries.cs
+++ Backend/AspNet-GraphQLDemoV2.Server/GraphQL/Types/Queries/MountainQueries.cs
@@ -14,7 +14,6 @@ namespace AspNet_GraphQLDemoV2.Server.GraphQL.Types.Queries
         //}
 
         [UseFiltering]
-        [UsePaging]
         public async Task<List<Mountain>> GetMountains([Service] MountainDbContext mountainDb)
         {
             return await mountainDb.Mountains.ToListAsync();
---
+++ Client/AspNetGraphQLDemoV2.Client/Program.cs
@@ -0,0 +1,12 @@
+using AspNetGraphQLDemoV2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddDemoClient();
+await builder.Build().RunAsync();
