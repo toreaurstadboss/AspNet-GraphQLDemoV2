3af83d336da75f75ae4235aa8da403f294900c6c
Tore Aurstad <tore.aurstad@hemit.no>
2022-10-21 14:38:11 +0200

    Added Blazorise and Blazorise.DataGrid

--- Client/AspNetGraphQLDemoV2.Client/Program.cs
+++ Client/AspNetGraphQLDemoV2.Client/Program.cs
@@ -1,4 +1,7 @@
 using AspNetGraphQLDemoV2.Client;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +11,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorise(options =>
+  {
+      options.Immediate = true;
+  })
+    .AddBootstrapProviders()
+    .AddFontAwesomeIcons();
 builder.Services.AddGraphQLDemoClient().ConfigureHttpClient(client => client.BaseAddress = new Uri("https://localhost:7074/graphql"));
 await builder.Build().RunAsync();
